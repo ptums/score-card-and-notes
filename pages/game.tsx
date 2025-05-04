bd5edd7f66eecd298bba6eb7711f64cb97ccadbb
ptums <ptumulty923@gmail.com>
2025-05-04 09:35:56 -0400

    hide import

--- pages/game.tsx
+++ pages/game.tsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { db, Score } from "../lib/db";
 import Link from "next/link";
-import BottomSheet from "@/components/BottomSheet";
 
 type Rating = 0 | 1 | 2 | 3 | 4;
 
